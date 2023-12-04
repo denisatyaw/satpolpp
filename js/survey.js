3d30a6e06522479e512f90205de9aef016dbf893
denisatyaw <fareeldeniz11@gmail.com>
2023-12-05 04:07:12 +0700

    upload

---
+++ js/survey.js
@@ -0,0 +1,18 @@
+
+const setStep = step => {
+	document.querySelectorAll(".step-content").forEach(element => element.style.display = "none");
+	document.querySelector("[data-step='" + step + "']").style.display = "block";
+	document.querySelectorAll(".steps .step").forEach((element, index) => {
+		index < step-1 ? element.classList.add("complete") : element.classList.remove("complete");
+		index == step-1 ? element.classList.add("current") : element.classList.remove("current");
+	});
+};
+document.querySelectorAll("[data-set-step]").forEach(element => {
+	element.onclick = event => {
+		event.preventDefault();
+		setStep(parseInt(element.dataset.setStep));
+	};
+});
+// <?php if (!empty($_POST)): ?>
+// setStep(4);
+// <?php endif; ?>
